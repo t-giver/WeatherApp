085656b7bfa1e53170a2bf5755778938aaaaec21
spark-05 <>
2024-05-23 13:09:05 +0900

    API作業完了

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var resultView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func btnReload(_ sender: Any) {
+        setWeatherType()
+    }
+    
+    func setWeatherType(){
+        let fetchWeatherString = YumemiWeather.fetchWeatherCondition()
+        if let image = UIImage(named: fetchWeatherString)?.withRenderingMode(.alwaysTemplate) {
+            resultView.image = image
+            switch fetchWeatherString {
+            case "sunny":
+                resultView.tintColor = .red
+            case "cloudy":
+                resultView.tintColor = .gray
+            case "rainy":
+                resultView.tintColor = .blue
+            default:
+                resultView.tintColor = .black
+            }
+            
+        }
+        
+    }
 }
-
