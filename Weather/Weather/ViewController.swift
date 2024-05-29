cd28accc6b4fa068479cd6ef0c404f033a65d33d
spark-05 <>
2024-05-29 15:37:58 +0900

    08課題完了

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -13,54 +13,65 @@ class ViewController: UIViewController {
     let setWeather = SetWeather()
     var delegate: weatherDateSet?
     
+    
     @IBOutlet weak var resultView: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setWeather.delegate = self
+        self.indicator.hidesWhenStopped = true
+        
     }
     
     @IBAction func btnReload(_ sender: Any) {
         setWeather.wetherDate()
+        self.indicator.startAnimating()
     }
-    
     @IBAction func closebtn(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    @IBOutlet weak var minTemperature: UILabel!
     
+    @IBOutlet weak var minTemperature: UILabel!
     @IBOutlet weak var maxTemperature: UILabel!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
 }
-
-
 
 extension ViewController: weatherDateSet {
-    
     func weatherDateCollection(weather: Weather) {
-        if let image = UIImage(named: weather.weatherCondition)?.withRenderingMode(.alwaysTemplate) {
-            resultView.image = image
+        DispatchQueue.main.async{
+            if let image = UIImage(named: weather.weatherCondition)?.withRenderingMode(.alwaysTemplate) {
+                self.resultView.image = image
+            }
+            switch weather.weatherCondition {
+            case "sunny":
+                self.resultView.tintColor = .red
+            case "cloudy":
+                self.resultView.tintColor = .gray
+            case "rainy":
+                self.resultView.tintColor = .blue
+            default:
+                break
+            }
+            self.minTemperature.text = String(weather.minTemp)
+            self.maxTemperature.text = String(weather.maxTemp)
+            self.indicator.stopAnimating()
+            self.indicator.isHidden = true
         }
-        switch weather.weatherCondition {
-        case "sunny":
-            resultView.tintColor = .red
-        case "cloudy":
-            resultView.tintColor = .gray
-        case "rainy":
-            resultView.tintColor = .blue
-        default:
-            break
-        }
-        minTemperature.text = String(weather.minTemp)
-        maxTemperature.text = String(weather.maxTemp)
+        
     }
-    
     func wetherError(message:String) {
-        let alert = UIAlertController(title:message, message: "もう一度お試しください", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+        DispatchQueue.main.async{
+            let alert = UIAlertController(title:message, message: "もう一度お試しください", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            self.indicator.stopAnimating()
+        }
     }
     
-
+    
 }
+
 
--- Weather/Weather/weather.swift
+++ Weather/Weather/weather.swift
@@ -18,29 +18,32 @@ protocol weatherDateSet {
 
 class SetWeather {
     var delegate: weatherDateSet?
-    func wetherDate() {
-        let sendJsonString = Date(area: "Tokyo", date: "2020-04-01T12:00:00+09:00")
-        
-        do {
-            let encoder = JSONEncoder()         //エンコード
-            let jsonData = try encoder.encode(sendJsonString)
-            guard let jsonString = String(data: jsonData, encoding: .utf8)
-            else {
-                return
-            }
+        func wetherDate() {
+            DispatchQueue.global().async{
+            let sendJsonString = Date(area: "Tokyo", date: "2020-04-01T12:00:00+09:00")
             
-            let fetchWeatherString = try YumemiWeather.fetchWeather(jsonString)
-            guard let jsonString = fetchWeatherString.data(using: .utf8)            //データ変換
-            else{
-                return
+            do {
+                let encoder = JSONEncoder()         //エンコード
+                let jsonData = try encoder.encode(sendJsonString)
+                guard let jsonString = String(data: jsonData, encoding: .utf8)
+                else {
+                    return
+                }
+                
+                let fetchWeatherString = try YumemiWeather.syncFetchWeather(jsonString)
+                guard let jsonString = fetchWeatherString.data(using: .utf8)            //データ変換
+                else{
+                    return
+                }
+                
+                let decoder = JSONDecoder()             //デコード
+                let weatherStruct = try decoder.decode(Weather.self, from: jsonString)
+                self.delegate?.weatherDateCollection(weather: weatherStruct)
             }
-            
-            let decoder = JSONDecoder()             //デコード
-            let weatherStruct = try decoder.decode(Weather.self, from: jsonString)
-            self.delegate?.weatherDateCollection(weather: weatherStruct)
-        }
-        catch {
-            self.delegate?.wetherError(message: "エラーだよ")
+            catch {
+                self.delegate?.wetherError(message: "エラーだよ")
+            }
         }
     }
 }
+
